b8f2718a61e3ad3984c3a7f0b11bf641b475a0e4
Matthew Maravilla <matthewmaravilla@Matthews-Laptop.local>
2024-02-15 20:17:43 -0500

    initial commit

---
+++ app/src/main/java/com/swe6673/finance/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.swe6673.finance.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
