9daa5996d8c964e5e7224d4fa2a68bb5f2ba6331
samueltos <samueltos@yahoo.com>
2024-04-28 18:44:20 -0600

    new commit

--- app/src/test/java/com/swe6673/finance/controller/UserControllerTest.java
+++ app/src/test/java/com/swe6673/finance/controller/UserControllerTest.java
@@ -1,69 +1,97 @@
 package com.swe6673.finance.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.swe6673.finance.config.AppConfig;
 import com.swe6673.finance.resource.User;
 import com.swe6673.finance.resource.UserDTO;
+import com.swe6673.finance.service.UserService;
 import com.swe6673.finance.testbuilder.UserTestBuilder;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = AppConfig.class)
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
 
     private static final String API_URLPATH = "/api/user";
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private UserService userService;
+
 
     @Test
     public void shouldCreateNewUserSuccessfully() throws Exception{
         User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        this.mockMvc.perform(post(API_URLPATH, mockedUser)).andExpect(status().isCreated());
+        String requestBody = objectMapper.writeValueAsString(mockedUser);
+        Mockito.when(userService.createUser(Mockito.any(User.class))).thenReturn(mockedUser);
+        this.mockMvc.perform(post(API_URLPATH).contentType("application/json").content(requestBody))
+                .andExpect(status().isCreated());
     }
 
     @Test
     public void shouldFailCreateNewUser() throws Exception{
         User mockedUser = UserTestBuilder.builder().userId(null).build();
-        this.mockMvc.perform(post(API_URLPATH, mockedUser)).andExpect(status().is5xxServerError());
+        String requestBody = objectMapper.writeValueAsString(mockedUser);
+        this.mockMvc.perform(post(API_URLPATH).contentType("application/json").content(requestBody))
+                .andExpect(status().is5xxServerError());
     }
 
     @Test
     public void shouldUpdateUserSuccessfully() throws Exception{
         User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        this.mockMvc.perform(put(API_URLPATH, mockedUser)).andExpect(status().isOk());
+        String requestBody = objectMapper.writeValueAsString(mockedUser);
+        Mockito.when(userService.createUser(Mockito.any(User.class))).thenReturn(mockedUser);
+
+        this.mockMvc.perform(put(API_URLPATH).contentType("application/json").content(requestBody))
+                .andExpect(status().isOk());
     }
 
     @Test
     public void shouldFailUpdateUser() throws Exception{
         User mockedUser = UserTestBuilder.builder().userId(null).build();
-        this.mockMvc.perform(put(API_URLPATH, mockedUser)).andExpect(status().is5xxServerError());
+        String requestBody = objectMapper.writeValueAsString(mockedUser);
+        this.mockMvc.perform(put(API_URLPATH).contentType("application/json").content(requestBody))
+                .andExpect(status().is5xxServerError());
     }
 
     @Test
     public void shouldDeleteUserSuccessfully() throws Exception{
-        String userId = "124322";
-        this.mockMvc.perform(delete(API_URLPATH + "/delete/{id}", userId)).andExpect(status().isOk());
+        User mockedUser = UserTestBuilder.builder().userId("124322").build();
+        String requestBody = objectMapper.writeValueAsString(mockedUser);
+        Mockito.when(userService.createUser(Mockito.any(User.class))).thenReturn(mockedUser);
+        this.mockMvc.perform(delete(API_URLPATH + "/delete/" + mockedUser.getUserId())).andExpect(status().isOk());
     }
 
     @Test
     public void shouldFailDeleteUser() throws Exception{
         String userId = "null";
-        this.mockMvc.perform(delete(API_URLPATH + "/delete/{id}", userId)).andExpect(status().isNotFound());
+        this.mockMvc.perform(delete(API_URLPATH + "/delete/" + userId)).andExpect(status().isNotFound());
     }
 
     @Test
     public void shouldGetUserSuccessfully() throws Exception{
-        String userId = "124322";
-        this.mockMvc.perform(get(API_URLPATH, userId)).andExpect(status().isOk());
+        User mockedUser = UserTestBuilder.builder().userId("124322").build();
+        String id = mockedUser.getUserId();
+        Mockito.when(userService.getUser(id)).thenReturn(mockedUser);
+        this.mockMvc.perform(get(API_URLPATH+"/"+id)).andExpect(status().isOk());
     }
 
     @Test
@@ -77,6 +105,7 @@ public class UserControllerTest {
         UserDTO userDTO = new UserDTO();
         userDTO.setEmail(mockedUser.getEmailAddress());
         userDTO.setPassword(mockedUser.getPassword());
+        Mockito.when(userService.login(userDTO)).thenReturn(mockedUser);
         this.mockMvc.perform(get(API_URLPATH + "/login", userDTO)).andExpect(status().isOk());
     }
 
@@ -93,6 +122,7 @@ public class UserControllerTest {
         UserDTO userDTO = new UserDTO();
         userDTO.setEmail(mockedUser.getEmailAddress());
         userDTO.setPassword(mockedUser.getPassword());
+        Mockito.when(userService.logout(userDTO)).thenReturn("successfully log out");
         this.mockMvc.perform(post(API_URLPATH + "/logout", userDTO)).andExpect(status().isOk());
     }
 
--- app/src/test/java/com/swe6673/finance/service/UserServiceTest.java
+++ app/src/test/java/com/swe6673/finance/service/UserServiceTest.java
@@ -11,7 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,76 +25,74 @@ public class UserServiceTest {
     @Mock
     public UserRepository userRepository;
 
-    @Mock
-    private ModelMapper modelMapper;
 
     @Test
     public void testGetUserSuccessfully(){
         User user = new User("124322", "matos", "Project", "matos@project.com", "Admin_123");
 
-        User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        Mockito.when(userRepository.findById(mockedUser.getUserId()));
-        userService.getUser(user.getUserId());
-        assertEquals(mockedUser, user);
+        Mockito.when(userRepository.findById(user.getUserId())).thenReturn(Optional.of(user));
+        User mockedUser = userService.getUser(user.getUserId());
+        assertEquals(mockedUser, user, "User found successfully");
     }
+
 
     @Test
     public void testGetUserFail(){
-        User user = new User(null, "matos", "cloud", "matos@project.com", "Admin_123");
-
-        User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        Mockito.when(userRepository.findById(mockedUser.getUserId()));
-        userService.getUser(user.getUserId());
-        assertEquals(mockedUser, user, "Invalid user Id");
+        Mockito.when(userRepository.findById(null)).thenThrow(NullPointerException.class);
+        User mockedUser = userService.getUser(null);
+        assertEquals(null, mockedUser);
     }
+
+
     @Test
     public void testAddNewUserSuccessfully(){
         User user = new User("124322", "matos", "Project", "matos@project.com", "Admin_123");
 
-        User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        Mockito.when(userRepository.save(mockedUser));
-        userService.createUser(user);
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        User mockedUser = userService.createUser(user);
         assertEquals(mockedUser, user);
     }
+
 
     @Test
     public void testAddNewUserFail(){
         User user = new User(null, null, null, "matos@project.com", null);
 
-        User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        Mockito.when(userRepository.save(mockedUser));
-        userService.createUser(user);
+        Mockito.when(userRepository.save(user));
+        User mockedUser =  userService.createUser(null);
         assertEquals(mockedUser, user, "Invalid data and data type");
     }
+
 
     @Test
     public void testUpdateUserSuccessfully(){
         User user = new User("124322", "matos", "Project", "matos@project.com", "Admin_123");
 
-        User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        Mockito.when(userRepository.save(mockedUser));
-        userService.updateUser(user);
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        User mockedUser = userService.updateUser(user);
         assertEquals(mockedUser, user);
     }
 
+
     @Test
     public void testUpdateUserFail(){
-        User user = new User(null, "matos", "cloud", "matos@project.com", "Admin_123");
+        User user = new User(null, null, null, "matos@project.com", "Admin_123");
 
-        User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        Mockito.when(userRepository.save(mockedUser));
-        userService.updateUser(user);
+        Mockito.when(userRepository.save(user));
+        User mockedUser = userService.updateUser(user);
         assertEquals(mockedUser, user, "Invalid user id and data type");
     }
+
 
     @Test
     public void testDeleteUserSuccessfully(){
         User user = new User("124322", "matos", "Project", "matos@project.com", "Admin_123");
-
-        User mockedUser = UserTestBuilder.builder().userId("124322").build();
-        userService.deleteUser(user.getUserId());
-        assertEquals(mockedUser, user);
+        String id = user.getUserId();
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        String response = userService.deleteUser(id);
+        assertEquals("Deleted Successfully", response);
     }
+
 
     @Test
     public void testDeleteUserFail(){
@@ -104,22 +103,25 @@ public class UserServiceTest {
         assertEquals(mockedUser, user, "Invalid User Id");
     }
 
+
     @Test
     public void testLoginUserSuccessfully(){
         User user = new User("124322", "matos", "Project", "matos@project.com", "Admin_123");
 
-        User mockedUser = UserTestBuilder.builder().userId("124322").build();
         UserDTO userDTO = new UserDTO();
         userDTO.setEmail(user.getEmailAddress());
         userDTO.setPassword(user.getPassword());
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        Mockito.when(userRepository.findById(user.getUserId())).thenReturn(Optional.of(user));
 
-        userService.login(userDTO);
-        assertEquals(mockedUser, user);
+        User mockedUser = userService.login(userDTO);
+        assertEquals(mockedUser, user, "login successfully");
     }
+
 
     @Test
     public void testLoginUserFail(){
-        User user = new User(null, "matos", "cloud", null, null);
+        User user = new User(null, null, "cloud", null, null);
 
         User mockedUser = UserTestBuilder.builder().userId("124322").build();
         UserDTO userDTO = new UserDTO();
@@ -127,7 +129,7 @@ public class UserServiceTest {
         userDTO.setPassword(user.getPassword());
 
         userService.login(userDTO);
-        assertEquals(mockedUser, user, "Invalid username and password");
+        assertEquals(null, user, "Invalid username and password");
     }
 
 }
